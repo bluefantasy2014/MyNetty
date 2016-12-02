b687c69d5cb6a6a24ba8487c01cddd604d89a549
Shi Jijun <5080053@qq.com>
2016-12-02 11:00:11 +0800

    test

---
+++ httpserver/src/main/java/netty/jerry/test/ByteBufUtilTest.java
@@ -0,0 +1,25 @@
+package netty.jerry.test;
+
+import org.apache.log4j.PropertyConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
+import io.netty.buffer.Unpooled;
+
+public class ByteBufUtilTest {
+private static final Logger LOG = LoggerFactory.getLogger(HeapByteBufTest.class);
+	
+	public static void main(String [] args){
+		PropertyConfigurator.configure("log4j.properties");
+		byte[] data = {(byte) 255,2,3,4}; 
+		
+		ByteBuf heapBuf = Unpooled.wrappedBuffer(data);  
+		LOG.info(ByteBufUtil.hexDump(heapBuf));  
+		
+		String name = "史纪军";
+		heapBuf = Unpooled.wrappedBuffer(name.getBytes());  
+		LOG.info(ByteBufUtil.hexDump(heapBuf)); 
+	}
+}
--- httpserver/src/main/java/netty/jerry/test/DirectByteBufTest.java
+++ httpserver/src/main/java/netty/jerry/test/DirectByteBufTest.java
@@ -1,5 +1,26 @@
 package netty.jerry.test;
 
+import org.apache.log4j.PropertyConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+
 public class DirectByteBufTest {
+
+	private static final Logger LOG = LoggerFactory.getLogger(HeapByteBufTest.class);
+	
+	public static void main(String [] args){
+		PropertyConfigurator.configure("log4j.properties");
+		byte[] data = {1,2,3,4}; 
+		
+		ByteBuf directBuf = Unpooled.directBuffer(100);  
+		LOG.debug("Whether the directBuf is array-backed? :" + directBuf.hasArray());
+		ByteBuf copiedBuf = Unpooled.copiedBuffer(data); 
+		LOG.debug("Whether the copiedBuf is array-backed? :" + copiedBuf.hasArray());
+		ByteBuf wrappedBuf = Unpooled.wrappedBuffer(data);  
+		LOG.debug("Whether the wrappedBuf is array-backed? :" + wrappedBuf.hasArray());
+	}
 
 }
