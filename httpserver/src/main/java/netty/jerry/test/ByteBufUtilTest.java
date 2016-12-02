f1d2a761c790fcb0cef6fe4d3767558aa18588cc
Shi Jijun <5080053@qq.com>
2016-12-02 11:50:54 +0800

    test

--- httpserver/src/main/java/netty/jerry/test/ByteBufUtilTest.java
+++ httpserver/src/main/java/netty/jerry/test/ByteBufUtilTest.java
@@ -18,6 +18,11 @@ private static final Logger LOG = LoggerFactory.getLogger(HeapByteBufTest.class)
 		ByteBuf heapBuf = Unpooled.wrappedBuffer(data);  
 		LOG.info(ByteBufUtil.hexDump(heapBuf));  
 		
+		byte[] array = heapBuf.array(); 
+		for (int i=0; i<array.length; ++i){
+			System.out.println(array[i]);
+		}
+		
 		String name = "史纪军";
 		heapBuf = Unpooled.wrappedBuffer(name.getBytes());  
 		LOG.info(ByteBufUtil.hexDump(heapBuf)); 
