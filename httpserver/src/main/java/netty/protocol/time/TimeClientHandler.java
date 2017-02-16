208ee8dd7ac664f108b2968b825093c44aacd607
Shi Jijun <5080053@qq.com>
2017-02-16 16:25:50 +0800

    Time protocol

---
+++ httpserver/src/main/java/netty/protocol/time/TimeClientHandler.java
@@ -0,0 +1,30 @@
+package netty.protocol.time;
+
+import java.util.Date;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class TimeClientHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf m = (ByteBuf) msg; // (1)
+        try {
+            long currentTimeMillis = (m.readUnsignedInt() - 2208988800L) * 1000L;
+            System.out.println(new Date(currentTimeMillis));
+            //JerryQ: 如果服务端close了这个channel，但是Client端没有close的话，这个channel还是被close了。怎么回事？及时注释掉这一行，client还是
+            //会发出FIN命令，为何？ 
+            ctx.close();  
+        } finally {
+            m.release();
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
---
+++ httpserver/src/main/java/netty/protocol/time/TimeServerHandler.java
@@ -0,0 +1,40 @@
+package netty.protocol.time;
+
+import org.apache.log4j.Logger;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class TimeServerHandler extends ChannelInboundHandlerAdapter  {
+
+	private static final Logger LOG = Logger.getLogger(TimeServerHandler.class);
+	
+
+    @Override
+	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+    	final ByteBuf time = ctx.alloc().buffer(4); // (2)
+        time.writeInt((int) (System.currentTimeMillis() / 1000L + 2208988800L));
+
+        final ChannelFuture f = ctx.writeAndFlush(time); // (3)
+        f.addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture future) {
+                assert f == future;
+                ctx.close();
+            }
+        }); // (4)
+	}
+
+
+	@Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
